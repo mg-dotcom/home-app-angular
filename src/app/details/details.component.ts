25509ad598fa7ce0a9847bc78f1452e990e55517
mg.apisara <apisara.chon@mail.kmutt.ac.th>
2025-01-04 18:09:58 +0700

    Service added

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,52 @@
+import { Component, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ActivatedRoute } from "@angular/router";
+import { HousingService } from "../housing.service";
+import { HousingLocation } from "../housing-location";
+
+@Component({
+  selector: "app-details",
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <article>
+      <img class="listing-photo" [src]="housingLocation?.photo" />
+      <section class="listing-description">
+        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
+        <p class="listing-location">
+          {{ housingLocation?.city }}, {{ housingLocation?.state }}
+        </p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this housing location</h2>
+        <ul>
+          <li>Units available: {{ housingLocation?.availableUnits }}</li>
+          <li>
+            Does this Location have wifi:
+            {{ housingLocation?.wifi ? "Yes" : "No" }}
+          </li>
+          <li>
+            Does this Location have laundry:
+            {{ housingLocation?.laundry ? "Yes" : "No" }}
+          </li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+        <h2 class="section-heading">Apply now to live here</h2>
+        <button class="primary" type="button">Apply Now</button>
+      </section>
+    </article>
+  `,
+  styleUrls: ["./details.component.css"],
+})
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
+
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.paramMap.get("id"));
+    this.housingLocation =
+      this.housingService.getHousingLocationById(housingLocationId);
+  }
+}
